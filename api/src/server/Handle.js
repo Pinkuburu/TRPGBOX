173c9a25cb219451e7310fff80fd904fd4c464bb
hoten01 <153023861@qq.com>
2017-05-09 22:53:05 +0800

    restart

--- api/src/server/Handle.js
+++
@@ -1,6 +0,0 @@
-const requestHandlers = require('./HandleRequest');
-let handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-
-module.exports = handle;
--- index.js
+++
@@ -1,6 +0,0 @@
-const server = require('./api/src/server/Server');
-const route = require('./api/src/server/Route');
-const handle = require('./api/src/server/Handle');
-
-
-server.start(route.route,handle);
